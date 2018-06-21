0f7dd22a880e74d77b2084fe6e0a62f1aca41610
laurentum <32101926+laurentum@users.noreply.github.com>
2018-06-20 23:40:37 -0400

    Update robot_obrero_a.js

--- robot_obrero_a.js
+++ robot_obrero_a.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         EpeniBot LowCost
-// @version      1.16a
-// @description  Este es el robot ahorrador. En esta versión se detecta el "timer running"
+// @name         Robot DLV sin RP
+// @version      1.48a
+// @description  Sin bouncer ni canje de puntos RP
 // @author       laurentum
 // @match        https://freebitco.in/*
 // @grant        none
@@ -12,7 +12,7 @@
 (function() {
 	'use strict';
 
-	var version="1.16a";
+	var version="1.48a";
 
 	// función para consultar tiempo restante hasta próximo roll
 	function tiemporestante(){
@@ -30,71 +30,72 @@
 
 	// función para reportar
 	function Reportar(estatus) {
-	estatus=encodeURIComponent(estatus);
+		estatus=encodeURIComponent(estatus);
 		var parametros="Id="+userID+"&Btc="+balance_BTC+"&Rp="+balance_PR+"&Status="+estatus+"&Version="+version;
-		$.ajax({
+		peticion=$.ajax({
 			crossDomain: true,	
-        	url: "https://script.google.com/macros/s/AKfycbzYjk5fPj2IBAIVTQ17WfoF7Go-Ct6DPg1y3lzzrE6lnB6umRQ/exec?"+parametros,
+        		url: "https://script.google.com/macros/s/AKfycbzrBiC5Of2eAGPxoLBVFqcQ6W9mTu0N9Y3b2JWCTLYoeZ2s6npG/exec?"+parametros,
 			method : "GET",
-        	dataType: "jsonp"
-    	});
-	}
+        		dataType: "jsonp"
+    		});
+		// Callback handler en caso de éxito
+		peticion.done(function (response, textStatus, jqXHR){
+			// Pasa un mensaje a la consola
+			if (response["fila"]!=-1) {			
+				console.log("Escribiendo en la fila ",response["fila"]);
+			} else {
+				console.log("Maquina no está en mis registros");
+			}
+		});
+	} // Reportar
 
-	// premios es un objeto con una función para activar bonos para
-	// acrecentar rápidamente los puntos reward y cobrar el bono
-	// de aumento premio por lanzamiento cuando tengas suficientes puntos.
-	var premios = {};
-	premios.rutina = function() {
-        premios.puntos = parseInt($('.user_reward_points').text().replace(',',""));
-        premios.temporizadorbono = {};
-        if ($("#bonus_container_free_points").length !== 0) {
-            premios.temporizadorbono.texto = $('#bonus_span_free_points').text();
-            premios.temporizadorbono.hora = parseInt(premios.temporizadorbono.texto.split(":")[0]);
-            premios.temporizadorbono.minuto = parseInt(premios.temporizadorbono.texto.split(":")[1]);
-            premios.temporizadorbono.segundo = parseInt(premios.temporizadorbono.texto.split(":")[2]);
-            premios.temporizadorbono.actual = premios.temporizadorbono.hora * 3600 + premios.temporizadorbono.minuto * 60 + premios.temporizadorbono.segundo;
-        } else
-            premios.temporizadorbono.actual = 0;
-        if (premios.temporizadorbono.actual === 0 & tiemporestante()===0) {
-			if (premios.puntos>101200) {
-				document.getElementById("encash_points_number").value="100000";
-				RedeemRPProduct('cash');
-				RedeemRPProduct('free_points_100');
-				Reportar("Canjeando puntos por satoshis + bono de 100RP.");
+	// Esta es la función principal que hace todo
+	// Se ejecuta 2 seg después esperando los resultados del rebote
+	
+	function accion_principal() {
+		body.prepend(
+			$('<div/>').attr('style',"position:fixed;top:45px;left:0;z-index:999;width:300px;background-color:"+color_robot+";color: white; text-align: left;")
+			.append(
+				$('<div/>').attr('id','autochorrito')
+				.append($('<p/>').attr('style','text-decoration:bold; text-align:center').text("( ͡° ͜ʖ ͡°) ╭∩╮  v."+version))
+				.append($('<p/>').attr('style','text-decoration:bold; text-align:center').text("───────────────────────"))
+				.append($('<p/>').text(userID))
+				.append($('<p/>').text(acct_email))
+				.append($('<p/>').text(estado))
+				.append($('<p/>').text(estado_captcha))
+			)
+		).prepend($('<style/>').text("#autochorrito p { margin: 0; margin-left: 2px;  text-align: left; }"));
+		// activa la rutina para que se ejecute repetidamente de manera asíncrona según condiciones:
+		if (!hay_captcha & !bloqueo_ip & !timer_running) {	
+			var timeout=Math.floor(Math.random() * 60 )*1000+2000; //timeout adicional entre 2 y 62 segundos
+			if ($('#free_play_form_button').is(':visible')) {
+				console.log("Cobrando el premio.");
+				setTimeout(function(){ $('#free_play_form_button').click();},timeout);
 			}
-			else if (premios.puntos>1200) {
-				Reportar("Activando el bono de 100RP por lanzamiento.");
-				RedeemRPProduct('free_points_100');
-			}
-			else if (premios.puntos>600) {
-				Reportar("Activando el bono de 50RP por lanzamiento.");
-				RedeemRPProduct('free_points_50');
-			}
-			else if (premios.puntos>120) {
-				Reportar("Activando el bono de 10RP por lanzamiento.");
-				RedeemRPProduct('free_points_10');
-			}
-			else if (premios.puntos>20) {
-				Reportar("Activando el bono de 1RP por lanzamiento.");
-				RedeemRPProduct('free_points_1');
-			}
+			setTimeout(function(){
+				if ($('.close-reveal-modal').is(':visible')) {
+					setTimeout(function(){$('.close-reveal-modal').click();},1000);
+				}
+			},timeout+12000); // cierra la ventana de dialogo pop-up 12 segundos despues de jugar el chorrito
+			setTimeout(function(){location.reload(true);},timeout+3601000); //obliga a hacer un refrescamiento de la pagina en una hora
+			Reportar(estatus_reporte); // manda el reporte cada hora
+		} else {
+			setTimeout(function(){location.reload();},3600000); // nos vemos en una hora.
+			if (hay_captcha) estatus_reporte="Balance al día (captcha)";
+			if (timer_running) estatus_reporte="Balance al día (timer running)";
+			if (bloqueo_ip) estatus_reporte="Balance al día (bloqueo ip)";
+			Reportar(estatus_reporte); // reportar cada hora de todas formas
 		}
-	};
+	} // accion_principal
 
 	var body = $('body');
 	// datos de esta cuenta
 	var userID = (((document.getElementById('edit_tab')).getElementsByTagName('p')[0]).getElementsByTagName('span')[1]).innerHTML;
 	userID = parseInt(userID);
-	var hora_reporte=userID % 24;
 	var balance_BTC = parseFloat(document.getElementById('balance').innerHTML);
 	var balance_PR = parseInt($('.user_reward_points').text().replace(',',""));
-	// el tipo siempre estará despierto.
-	var despierto=true;
-	var estado="";
-	var hora_actual=new Date();
-	hora_actual=hora_actual.getHours();
-	if (despierto) {estado="Estoy despierto.";}
-	else {estado="Estoy dormido.";}
+	var acct_email=document.getElementById('edit_profile_form_email').value;
+	var estado="Estoy despierto.";
 	// verifica si hay captcha u otras condiciones
 	var hay_captcha=($('#captchasnet_free_play_captcha').is(':visible'))||($('#free_play_recaptcha').is(':visible'));
 	var timer_running=($("#multi_acct_same_ip").is(":visible"));
@@ -104,59 +105,9 @@
 	if (hay_captcha) {estado_captcha="¡Maldita captcha! Reportando a mi amo..."; color_robot="#a40000";}
 	if (timer_running) {estado_captcha="El reloj está corriendo. Reportando a mi amo..."; color_robot="#a40000";}
 	if (!timer_running & !hay_captcha) {
-		if (despierto) {
-			estado_captcha="Voy a cobrar el chorrito";
-			if ($("#bonus_container_free_points").length !== 0) {estado_captcha+=".";}
-			else {estado_captcha+=" y también voy a activar bonos (si tengo suficientes RP).";}
-		} else {
-			estado_captcha="No hago un coño.";
-		}
+		estado_captcha="Voy a cobrar el chorrito";
+		if ($("#bonus_container_free_points").length !== 0) {estado_captcha+=".";}
+		else {estado_captcha+=" y también voy a activar bonos (si tengo suficientes RP).";}
 	}
-    body.prepend(
-        $('<div/>').attr('style',"position:fixed;top:45px;left:0;z-index:999;width:300px;background-color:"+color_robot+";color: white; text-align: left;")
-            .append(
-                $('<div/>').attr('id','autofaucet')
-					.append($('<p/>').attr('style','text-decoration:bold; text-align:center').text("( ͡° ͜ʖ ͡°) ╭∩╮  v."+version))
-					.append($('<p/>').attr('style','text-decoration:bold; text-align:center').text("───────────────────────"))
-                    .append($('<p/>').text(estado))
-                    .append($('<p/>').text(estado_captcha))
-            )
-    ).prepend($('<style/>').text("#autofaucet p { margin: 0; margin-left: 2px;  text-align: left; }"));
-
-	// activa la rutina para que se ejecute repetidamente de manera asíncrona según condiciones:
-	if (despierto & !hay_captcha & !bloqueo_ip & !timer_running) {	
-		setTimeout(premios.rutina,1000);  // activar en un segundo
-		// Luego de activar bonos, cobra el chorrito cuando sea posible.
-		// El cobro del chorrito se hace aproximadamente cada hora, con tiempos 
-		// de inter-cobro aleatorios para no levantar sospechas.
-		var timeout=Math.floor(Math.random() * 60 )*1000+2000; //timeout adicional entre 2 y 62 segundos
-   		if ($('#free_play_form_button').is(':visible')) {
-			console.log("Cobrando el premios.");
-   			setTimeout(function(){ $('#free_play_form_button').click();},timeout);
-		}
-   		setTimeout(function(){
-			if ($('.close-reveal-modal').is(':visible')) {
-				setTimeout(function(){$('.close-reveal-modal').click();},1000);
-			}
-		},timeout+12000); // cierra la ventana de dialogo pop-up 10 segundos despues de jugar el chorrito
-		if (hora_actual==hora_reporte) {Reportar("Balance al día");} // manda el reporte diario a las 12 del mediodía.
-	} else {
-		setTimeout(function(){location.reload();},3600000); // nos vemos en una hora.
-		if (hora_actual==hora_reporte) {	// manda el reporte diario a las 12 del mediodía.
-			if (hay_captcha) {
-				Reportar("Balance al día (captcha)");
-			} 
-			else if (timer_running) {
-				Reportar("Balance al día (timer running)");
-			}
-			else {Reportar("Balance al día.");}
-		}
-		else {  // reporta el problema
-			if (hay_captcha) {Reportar("captcha");}
-			else if (timer_running) {Reportar("timer running");}  
-        }
-	}
+	setTimeout(accion_principal,2000); // espera 2 seg para ver si está autorizado o no y ejecuta el resto.
 })();
-
-
-
